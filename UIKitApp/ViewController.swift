b2afeb723aad6f9b9a798210d5b4504a6e98320c
Mikhail <Stepanov>
2025-06-24 14:50:45 +0300

    Initial Commit

---
+++ UIKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitApp
+//
+//  Created by Mikhail Stepanov on 24.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
